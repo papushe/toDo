2c2ed70dd8a1d3dad21132a2c6046c5c8a088fa8
Papushe <papushe9@gmail.com>
2017-10-14 12:37:53 +0300

    added chat component add chat service

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -7,6 +7,7 @@ export class ChatService implements OnInit{
   constructor() { }
 
   // private url = 'http://localhost:5000';
+  // private url = 'https://papushetodo.herokuapp.com';
   private url = 'https://papushetodo.herokuapp.com:5000';
   private socket;
 
