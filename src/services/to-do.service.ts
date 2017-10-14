dcd7cfc142208c716d4ee22bad2d94d38edf1343
Papushe <papushe9@gmail.com>
2017-10-14 17:23:41 +0300

    added chat component add chat service

--- src/services/to-do.service.ts
+++ src/services/to-do.service.ts
@@ -15,8 +15,8 @@ export class ToDoService {
   allUserData:any;
   static title: any;
   constructor( private _http: Http) { }
-  // baseUrl = 'https://papushetodo.herokuapp.com';
-  baseUrl = 'http://localhost:4300';
+  baseUrl = 'https://papushetodo.herokuapp.com';
+  // baseUrl = 'http://localhost:4300';
 
   Page(){
     return {
